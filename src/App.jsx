a44293a8c6089ac6e6fbf6de64993f75b4bc3488
Joseph Fantuzzi <95318530+joseph-fantuzzi@users.noreply.github.com>
2023-03-08 18:21:06 -0500

    [1] - Search and Display Organization's Repo Data (#2)
    * search organizations repos functionality and data display in place
    
    * added api response error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,72 @@
 import { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
+const BASE_URL = "https://api.github.com";
+
 function App() {
+  const [repos, setRepos] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  /**
+   * Sets the search value state to the value within the input
+   * @param {*} e
+   */
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  /**
+   * Sends a get request to github api to retrieve repo data for that organization
+   * @param {*} e
+   */
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    setErrorMessage("");
+    try {
+      if (searchValue) {
+        const response = await axios.get(
+          `${BASE_URL}/orgs/${searchValue}/repos`
+        );
+        setRepos(response.data);
+        if (response.data.length === 0)
+          setErrorMessage("Organization is private");
+      }
+    } catch (err) {
+      setRepos([]);
+      setErrorMessage("Organization not found");
+      console.error(err);
+    }
+  };
+
   return (
-    <div>
+    <div className="text-center py-5">
       <h1 className="text-2xl">GitHub Organization Explorer</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          value={searchValue}
+          onChange={handleSearchChange}
+          className="border-2"
+        />
+        <button className="border-2">Search</button>
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+      </form>
+      <div>
+        {repos.map((repo, i) => {
+          return (
+            <div key={i} className="p-5 border-2 w-7/12 mx-auto">
+              <h1 className="text-xl underline">{repo.name}</h1>
+              <p>{repo.language}</p>
+              <p>{repo.description}</p>
+              <p>Star Count: {repo.stargazers_count}</p>
+              <p>Fork Count: {repo.forks_count}</p>
+              <p>Date Created: {repo.created_at}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
