43ee7214cf830a4375b83f1e54678b5b46668f5b
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2023-03-08 16:50:23 -0500

    configured tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1>GitHub Organization Explorer</h1>
+      <h1 className="text-2xl">GitHub Organization Explorer</h1>
     </div>
   );
 }
