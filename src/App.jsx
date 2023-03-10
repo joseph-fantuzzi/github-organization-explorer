45b6640d124392910e709dde3d5b5cc7aaf929e7
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2023-03-09 19:14:37 -0500

    organization route with nested repo route in place

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import axios from "axios";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./components/Home";
-import Commits from "./components/Commits";
+import Repo from "./components/Repo";
+import Organization from "./components/Organization";
 import "./App.css";
 
 const BASE_URL = "https://api.github.com";
@@ -10,9 +10,13 @@ const BASE_URL = "https://api.github.com";
 function App() {
   const [repos, setRepos] = useState([]);
   const [commits, setCommits] = useState([]);
-  const [currentOrganization, setCurrentOrganization] = useState("");
   const [searchValue, setSearchValue] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [orgNotFound, setOrgNotFound] = useState(false);
+  const [repoNotFound, setRepoNotFound] = useState(false);
+  const [zeroRepos, setZeroRepos] = useState(false);
+  const [zeroCommits, setZeroCommits] = useState(false);
+
+  const navigate = useNavigate();
 
   /**
    * Sets the search value state to the value within the input
@@ -23,38 +27,20 @@ function App() {
   };
 
   /**
-   * Sends a get request to github api to retrieve repo data for that organization
+   * Navigates the user to the specified organization route if a search value is specified
    * @param {*} e
    */
   const handleFormSubmit = async (e) => {
     e.preventDefault();
-    setErrorMessage("");
-    try {
-      if (searchValue) {
-        const response = await axios.get(
-          `${BASE_URL}/orgs/${searchValue}/repos`
-        );
-        setRepos(response.data);
-        if (response.data.length === 0) {
-          setErrorMessage("Organization is private");
-        } else {
-          setCurrentOrganization(searchValue);
-        }
-        setSearchValue("");
-      }
-    } catch (err) {
-      setRepos([]);
-      setErrorMessage("Organization not found");
-      console.error(err);
+    if (searchValue) {
+      setRepoNotFound(false);
+      setOrgNotFound(false);
+      setZeroRepos(false);
+      setZeroCommits(false);
+      navigate(`/${searchValue}`);
+      setSearchValue("");
     }
   };
-
-  //sorts organization's repos by star count in descending order
-  const sortedRepos = repos.sort((a, b) => {
-    if (a.stargazers_count < b.stargazers_count) return 1;
-    if (a.stargazers_count > b.stargazers_count) return -1;
-    return 0;
-  });
 
   return (
     <Routes>
@@ -62,22 +48,37 @@ function App() {
         path="/"
         element={
           <Home
-            sortedRepos={sortedRepos}
             searchValue={searchValue}
             handleSearchChange={handleSearchChange}
             handleFormSubmit={handleFormSubmit}
-            errorMessage={errorMessage}
           />
         }
       />
       <Route
-        path="/:repoName"
+        path="/:orgName"
         element={
-          <Commits
+          <Organization
+            BASE_URL={BASE_URL}
+            repos={repos}
+            setRepos={setRepos}
+            orgNotFound={orgNotFound}
+            setOrgNotFound={setOrgNotFound}
+            zeroRepos={zeroRepos}
+            setZeroRepos={setZeroRepos}
+          />
+        }
+      />
+      <Route
+        path="/:orgName/:repoName"
+        element={
+          <Repo
             commits={commits}
             setCommits={setCommits}
-            currentOrganization={currentOrganization}
             BASE_URL={BASE_URL}
+            repoNotFound={repoNotFound}
+            setRepoNotFound={setRepoNotFound}
+            zeroCommits={zeroCommits}
+            setZeroCommits={setZeroCommits}
           />
         }
       />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,4 @@
-import { Link } from "react-router-dom";
-
-const Home = ({
-  sortedRepos,
-  searchValue,
-  handleSearchChange,
-  handleFormSubmit,
-  errorMessage,
-}) => {
+const Home = ({ searchValue, handleSearchChange, handleFormSubmit }) => {
   return (
     <div className="text-center py-5">
       <h1 className="text-2xl">GitHub Organization Explorer</h1>
@@ -18,24 +10,7 @@ const Home = ({
           className="border-2"
         />
         <button className="border-2">Search</button>
-        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
       </form>
-      <div>
-        {sortedRepos.map((repo, i) => {
-          return (
-            <Link key={i} to={repo.name}>
-              <div className="p-5 border-2 w-7/12 mx-auto cursor-pointer hover:border-red-500">
-                <h1 className="text-xl underline">{repo.name}</h1>
-                <p>{repo.language}</p>
-                <p>{repo.description}</p>
-                <p>Star Count: {repo.stargazers_count}</p>
-                <p>Fork Count: {repo.forks_count}</p>
-                <p>Date Created: {repo.created_at}</p>
-              </div>
-            </Link>
-          );
-        })}
-      </div>
     </div>
   );
 };
