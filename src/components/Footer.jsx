604a391a4e49c07a3d17a47bbaa26558527c98db
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2023-03-09 19:49:16 -0500

    added footer, changed background color, and added poppins font

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <p className="text-sm font-light">
+      Designed and Created By Joseph Fantuzzi 2023
+    </p>
+  );
+};
+
+export default Footer;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,28 @@
 import { FiSearch } from "react-icons/fi";
+import Footer from "./Footer";
 
 const Home = ({ searchValue, handleSearchChange, handleFormSubmit }) => {
   return (
-    <div className="text-center py-5 min-h-[100svh]">
-      <h1 className="text-2xl">GitHub Organization Explorer</h1>
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          value={searchValue}
-          onChange={handleSearchChange}
-          className="border-2"
-          placeholder="Search Organization..."
-        />
-        <button className="border-2">
-          <FiSearch />
-        </button>
-      </form>
+    <div className="min-h-[100svh] flex flex-col justify-between items-center">
+      <div />
+      <div className="mb-20 flex flex-col items-center">
+        <h1 className="text-3xl">GitHub Organization Explorer</h1>
+        <form onSubmit={handleFormSubmit} className="flex items-center gap-1">
+          <input
+            type="text"
+            value={searchValue}
+            onChange={handleSearchChange}
+            className="border-2"
+            placeholder="Search Organization..."
+          />
+          <button className="border-2">
+            <FiSearch />
+          </button>
+        </form>
+      </div>
+      <div className="py-3">
+        <Footer />
+      </div>
     </div>
   );
 };
