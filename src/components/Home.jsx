7f08d59e640d5d36482ae3c1fd7836a1a012a18b
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2023-03-09 19:34:27 -0500

    added react-icons dep

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
+import { FiSearch } from "react-icons/fi";
+
 const Home = ({ searchValue, handleSearchChange, handleFormSubmit }) => {
   return (
-    <div className="text-center py-5">
+    <div className="text-center py-5 min-h-[100svh]">
       <h1 className="text-2xl">GitHub Organization Explorer</h1>
       <form onSubmit={handleFormSubmit}>
         <input
@@ -8,8 +10,11 @@ const Home = ({ searchValue, handleSearchChange, handleFormSubmit }) => {
           value={searchValue}
           onChange={handleSearchChange}
           className="border-2"
+          placeholder="Search Organization..."
         />
-        <button className="border-2">Search</button>
+        <button className="border-2">
+          <FiSearch />
+        </button>
       </form>
     </div>
   );
